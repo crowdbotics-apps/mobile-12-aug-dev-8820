edd09283f1b9aed1ece22ca1b303bcb533ccbeb5
Crowdbotics <team@crowdbotics.com>
2020-08-12 06:20:51 +0000

    Update components

--- src/features/BlankScreen09830/screens/blank.js
+++ src/features/BlankScreen09830/screens/blank.js
@@ -22,13 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {
-    Input_4: "",
-    Input_5: "",
-    Datepicker_6: new Date("08/12/2020"),
-    Toggle_9: true,
-    Input_10: ""
-  }
+  state = {}
 
   render = () => (
     <View
@@ -66,42 +60,11 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Button
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "center",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#3366FF",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        onPress={() => alert("Pressed!")}
-      >
-        Press me!
-      </Button>
-      <Text
+      <Icon
+        iconFont="Eva Design Icons"
+        name="star"
+        width={20}
+        height={20}
         style={{
           marginLeft: 0,
           marginRight: 0,
@@ -114,264 +77,23 @@ export class _Blank extends React.Component {
           overflow: "visible",
           textAlign: "left",
           verticalAlign: "baseline",
-          fontSize: 12,
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontSize: 20,
           color: "#000000",
           backgroundColor: "#ffffff",
           fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
           textDecorationLine: "none",
           textTransform: "none",
           lineHeight: 12,
           letterSpacing: 0
         }}
-      >
-        Sample text content
-      </Text>
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        textStyle={{ fontSize: 12, color: "#000000", textAlign: "left" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        value={this.state.Input_4}
-        onChangeText={nextValue => this.setState({ Input_4: nextValue })}
-      />
-      <Input
-        placeholder="Number Input Placeholder"
-        editable={true}
-        keyboardType="numeric"
-        textStyle={{ fontSize: 12, color: "#000000", textAlign: "left" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        value={this.state.Input_5}
-        onChangeText={nextValue => this.setState({ Input_5: nextValue })}
-      />
-      <Datepicker
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        date={this.state.Datepicker_6}
-        onSelect={nextValue => this.setState({ Datepicker_6: nextValue })}
-      />
-      <Slider
-        value={50}
-        minimumValue={0}
-        maximumValue={100}
-        step={1}
-        disabled={false}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      />
-      <Image
-        resizeMode="cover"
-        style={{
-          width: "100%",
-          height: 200,
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      />
-      <Toggle
-        text="switch ON/OFF"
-        activeColor="#409EFF"
-        inactiveColor="#C0CCDA"
-        disabled={false}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        checked={this.state.Toggle_9}
-        onChange={nextChecked => this.setState({ Toggle_9: nextChecked })}
-      />
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        multiline={true}
-        textStyle={{
-          height: 100,
-          fontSize: 12,
-          color: "#000000",
-          textAlign: "left"
-        }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        value={this.state.Input_10}
-        onChangeText={nextValue => this.setState({ Input_10: nextValue })}
       />
     </View>
   )
